fe58823e5bf8b159c9d562360517673bdd513404
GragN <98579784+GragN@users.noreply.github.com>
2022-10-07 06:17:47 +0900

    commit

---
+++ api_with_restrictions/advertisements/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from advertisements.models import Advertisement
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name',)
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator', 'status', 'created_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+        ad_user = self.context['request'].user
+        ad_quantity = Advertisement.objects.filter(status='OPEN', creator=ad_user)
+        if self.context['request'].method == 'POST' and len(ad_quantity) >= 10:
+            raise ValidationError('10 ad limit exceeded. you need to close unnecessary ads')
+        if self.context['request'].method == 'PATCH' and data.get('status') == 'OPEN' and len(ad_quantity) >= 10:
+            raise ValidationError('10 ad limit exceeded. you need to close unnecessary ads')
+        if self.context['request'].method == 'PUT' and data.get('status') == 'OPEN' and len(ad_quantity) >= 10:
+            raise ValidationError('10 ad limit exceeded. you need to close unnecessary ads')
+        return data
